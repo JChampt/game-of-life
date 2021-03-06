765f4ff2ad5a93567a66e93fca826e095622d320
JC <paolot55@gmail.com>
2021-03-06 12:52:06 -0800

    Added clear button Adjusted margins a bit Added clear button logic Refractored functions to group like tasks

--- script.js
+++ script.js
@@ -1,52 +1,77 @@
+let GRID;
+
 makeGrid(21);
+addControlListeners();
 
-const grid = Array.from(document.querySelector('#grid').children).map((row) =>
-  Array.from(row.children)
-);
-
+/* 
+TODO: makeGrid could/should be a class. The constructor could accept a size,
+update create the DOM elements and add them to the DOM, then return an 
+2d Array containing pointers to all of the cells;
+ */
 function makeGrid(size) {
   const grid = document.querySelector('#grid');
-  clearGrid();
+  deleteGrid();
   for (let i = 0; i < size; i++) {
     grid.appendChild(makeRow(size, i));
   }
+  GRID = mapGrid();
+
+  function deleteGrid() {
+    document.querySelector('#grid').innerHTML = '';
+  }
+
+  function makeRow(size, id) {
+    let row = document.createElement('div');
+    row.classList = 'row';
+    row.id = `r-${id}`;
+
+    for (let i = 0; i < size; i++) {
+      row.appendChild(makeCell(`${id}:${i}`));
+    }
+    return row;
+  }
+
+  function makeCell(id) {
+    let cell = document.createElement('input');
+    cell.type = 'checkbox';
+    cell.classList = 'cell';
+    cell.id = id;
+    cell.checked = random() ? true : false;
+    return cell;
+  }
+
+  function random() {
+    return Math.round(Math.random());
+  }
+
+  function mapGrid() {
+    return Array.from(document.querySelector('#grid').children).map((row) =>
+      Array.from(row.children)
+    );
+  }
+}
+
+function addControlListeners() {
+  const button = document.querySelector('#clear');
+  button.addEventListener('click', clearGrid);
 }
 
 function clearGrid() {
-  document.querySelector('#grid').innerHTML = '';
-}
-
-function makeRow(size, id) {
-  let row = document.createElement('div');
-  row.classList = 'row';
-  row.id = id;
-
-  for (let i = 0; i < size; i++) {
-    row.appendChild(makeCell(`${id}:${i}`));
+  for (let row = 0; row < GRID.length; row++) {
+    for (let col = 0; col < GRID[row].length; col++) {
+      const cell = GRID[row][col];
+      cell.checked = false;
+    }
   }
-  return row;
-}
-
-function makeCell(id) {
-  let cell = document.createElement('input');
-  cell.type = 'checkbox';
-  cell.classList = 'cell';
-  cell.id = id;
-  cell.checked = random() ? true : false;
-  return cell;
-}
-
-function random() {
-  return Math.round(Math.random());
 }
 
 function evalCell(cell) {
   const [row, col] = getCellLocation(cell);
   const leftBound = col - 1 < 0 ? col : col - 1;
 
-  let rowAbove = grid[row - 1] ? grid[row - 1].slice(leftBound, col + 2) : [];
-  let rowOn = grid[row].slice(leftBound, col + 2);
-  let rowBelow = grid[row + 1] ? grid[row + 1].slice(leftBound, col + 2) : [];
+  let rowAbove = GRID[row - 1] ? GRID[row - 1].slice(leftBound, col + 2) : [];
+  let rowOn = GRID[row].slice(leftBound, col + 2);
+  let rowBelow = GRID[row + 1] ? GRID[row + 1].slice(leftBound, col + 2) : [];
 
   let total = rowAbove.concat(rowOn, rowBelow).reduce((sum, ele) => {
     sum += ele.checked == true ? 1 : 0;
@@ -58,10 +83,10 @@ function evalCell(cell) {
   } else {
     return total == 3 ? 1 : 0;
   }
-}
 
-function getCellLocation(cell) {
-  return cell.id.split(':').map(Number);
+  function getCellLocation(cell) {
+    return cell.id.split(':').map(Number);
+  }
 }
 
 function makeRandomTemplate(size) {
@@ -71,8 +96,8 @@ function makeRandomTemplate(size) {
 
 function updateGrid(template) {
   for (let row = 0; row < template.length; row++) {
-    for (let col = 0; col < grid[row].length; col++) {
-      const cell = grid[row][col];
+    for (let col = 0; col < GRID[row].length; col++) {
+      const cell = GRID[row][col];
       cell.checked = template[row][col] ? true : false;
     }
   }
