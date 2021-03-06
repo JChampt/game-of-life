0ad99c797b12758e0798406470f96cbf5c7aae88
JC <paolot55@gmail.com>
2021-03-05 17:17:34 -0800

    Added cell clicked animation Refractored naming a bit for clarity Decided on flow of data structure Keeping DOM elements percistant and updating them

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-displayGrid(21);
+makeGrid(21);
 
-function displayGrid(size) {
+const grid = Array.from(document.querySelector('#grid').children).map((row) =>
+  Array.from(row.children)
+);
+
+function makeGrid(size) {
   const grid = document.querySelector('#grid');
-
   clearGrid();
   for (let i = 0; i < size; i++) {
     grid.appendChild(makeRow(size, i));
   }
 }
 
+function clearGrid() {
+  document.querySelector('#grid').innerHTML = '';
+}
+
 function makeRow(size, id) {
   let row = document.createElement('div');
   row.classList = 'row';
-  row.id = `row-${id}`;
+  row.id = id;
 
   for (let i = 0; i < size; i++) {
     row.appendChild(makeCell(`${id}:${i}`));
@@ -24,20 +31,37 @@ function makeCell(id) {
   let cell = document.createElement('input');
   cell.type = 'checkbox';
   cell.classList = 'cell';
-  cell.id = `cell-${id}`;
+  cell.id = id;
   cell.checked = random() ? true : false;
   return cell;
 }
 
-function clearGrid() {
-  document.querySelector('#grid').innerHTML = '';
+function random() {
+  return Math.round(Math.random());
 }
 
-function makeGrid(size) {
+function makeRandomTemplate(size) {
   let arr = Array(size).fill(Array(size).fill(0));
   return arr.map((row) => row.map((col) => random()));
 }
 
-function random() {
-  return Math.round(Math.random());
+function updateGrid(template) {
+  for (let row = 0; row < template.length; row++) {
+    for (let cell = 0; cell < grid[row].length; cell++) {
+      const c = grid[row][cell];
+      c.checked = template[row][cell] ? true : false;
+    }
+  }
+}
+
+// Code from: https://www.sitepoint.com/delay-sleep-pause-wait/
+async function animateGrid(nSteps = 30, msDelayBetween = 333, gridSize = 21) {
+  do {
+    makeGrid(gridSize);
+    await sleep(msDelayBetween);
+  } while (--nSteps > 0);
+}
+
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
