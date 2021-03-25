b2ebc0be363abfe39f2a940b96ee0b38f324850b
JC <paolot55@gmail.com>
2021-03-25 09:57:49 -0700

    Refractor styles to smaller modules
    Moved all style imports to app.scss.  Broke styles into smaller modules
    for each section of app.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import { responsiveMakeGrid } from './javascript/makeGrid';
 import { addControlListeners } from './javascript/addControlListeners';
-import './style/style.scss';
-import './style/controls.scss';
-import './style/howto.scss';
+import './style/app.scss';
 
 // Init program after the body html is built
 responsiveMakeGrid();
